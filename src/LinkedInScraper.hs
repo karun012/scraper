111d44d47d402edcf64b5ba3e1a840b4574a5105
Karun <karun012@gmail.com>
2014-08-23 20:23:27 -0700

    Added firstname and lastname

--- src/LinkedInScraper.hs
+++ src/LinkedInScraper.hs
@@ -3,22 +3,30 @@ module LinkedInScraper where
 
 import Types
 import Scraping
+import Data.String.Utils
 
+instance Scrapable Name where
+    scrapeFromPage page = uncurry Name $ ((head . map strip $ getValues page "#name .given-name"), (head . map strip $ getValues page "#name .family-name"))
 instance Scrapable [Skill] where
-    scrapeFromPage page = getValues page "#profile-skills .competency span"
+    scrapeFromPage page = map strip $ getValues page "#profile-skills .competency span"
  
 instance Scrapable [Experience] where
     scrapeFromPage page = map (uncurry Experience) $ zip (companies page) (positions page)
 
 instance Scrapable LinkedInProfile where
-    scrapeFromPage page = LinkedInProfile { skills = skillsScraped, experience = experienceScraped } 
-                            where skillsScraped = scrapeFromPage page
+    scrapeFromPage page = LinkedInProfile { 
+                                name = nameScraped,
+                                skills = skillsScraped, 
+                                experience = experienceScraped 
+                            } 
+                            where nameScraped = scrapeFromPage page
+                                  skillsScraped = scrapeFromPage page
                                   experienceScraped = scrapeFromPage page
 companies :: String -> [String]
-companies page = getValues page "#profile-experience .position .org"
+companies page = map strip $ getValues page "#profile-experience .position .org"
 
 positions :: String -> [String]
-positions page = getValues page "#profile-experience .position .title"
+positions page = map strip $ getValues page "#profile-experience .position .title"
 
 linkedInprofile :: User -> Maybe LinkedInProfile
 linkedInprofile user = case document user of
--- src/Types.hs
+++ src/Types.hs
@@ -5,7 +5,13 @@ type LinkedInUrl = String
 type Skill = String
  
 data User = User { document :: Maybe String }
-data LinkedInProfile = LinkedInProfile { skills :: [Skill], experience :: [Experience] } deriving (Show)
+
+data Name = Name { firstName :: String, lastName :: String} deriving (Show)
+data LinkedInProfile = LinkedInProfile { 
+    name :: Name,
+    skills :: [Skill], 
+    experience :: [Experience] 
+} deriving (Show)
 data Experience = Experience { company :: String, position :: String } deriving (Show)
 
 class Scrapable a where
